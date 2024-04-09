1be3a241d2a6f829e3a1740c8d415cfe985d82af
Wilmar Stiven Valencia Cardona <wilmar2545@gmail.com>
2024-04-09 11:49:36 -0500

    setting railway deploy

--- social_media/settings.py
+++ social_media/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']
 
@@ -171,6 +171,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-STATIC_ROOT = BASE_DIR / 'theme'  # <-- Updated!
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
